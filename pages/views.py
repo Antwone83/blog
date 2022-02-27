dc76cc4aaae2d8c75192b91f232bee9325977679
Antwone Adams <adams.antwone@yahoo.com>
2022-02-27 11:05:51 -0800

    Update commit

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from re import template
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = "pages_templates/home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "pages_templates/about.html"
+
+# Create your views here.
